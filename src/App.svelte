f58d735d0a517f5e8d386de6a00eb8b6052aeeab
areeb <areeb.malik@cooperativecomputing.com>
2022-08-05 13:34:00 +0500

    Svelte each loop

--- src/App.svelte
+++ src/App.svelte
@@ -1,84 +1,35 @@
 <script>
-	let first_name = 'Areeb';
-	let last_name = 'Malik';
-	let beltColor = 'red';
-
-	const changeColor = () => {
-		beltColor = 'red'
-	}
-
-	/** 
-	 * @param event
-	 * when we want an event like something happening on a button click then
-	 * we pass a parameter inside the function to fire the event
-	 */
-	const typeColor = (event) => {
-		beltColor = event.target.value;
-	}
+	let items = [
+		{name: 'Yoshi', belt_color: 'black', age: '28', id: '1'},
+		{name: 'Mario', belt_color: 'orange', age: '28', id: '2'},
+		{name: 'Luigi', belt_color: 'red', age: '28', id: '3'},
+		{name: 'Toad', belt_color: 'yellow', age: '28', id: '4'}
+	];
 </script>
 
 <main>
-	<!-- 
-		we can dynamically change colors by asking users to write their colors 
-	-->
-
-	<!-- this is how we show output -->
-	<h1 style="color: {beltColor}">
-		{first_name} {last_name} - {beltColor}
-	</h1>
-
-	<!-- 
-		by using the value attribute, we are binding this input with the default color we had defined in the code
-	-->
-
-	<p>
-		To change belt color please press this button <br><br> <button on:click="{changeColor}">Change color</button>
-		<!--
-			this is long way for 2 way binding and we will use a shorthand for 2 way binding
-			for 1 way data binding use the long way
-			<input type="text" value="{beltColor}" on:input="{typeColor}"> 
-		-->
-
-		<input type="text" bind:value={beltColor}>
-	</p>
-
-
-	<label for="first_name">
-		First name
-		<input type="text" id="first_name" bind:value={first_name}>
-	</label>
-	<br>
-
-	<label for="last_name">
-		Last name
-		<input type="text" id="last_name" bind:value={last_name}>
-	</label>
-	<br>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	<p>
-		for docs, <a href="https://svelte.dev/tutorial/basics" target="__blank">click me!</a>
-	</p>
+	<!-- we will use a loop to cycle and display the array data -->
+	{#each items as item (items.id)}
+		<!-- now we difine the html template for the output -->
+		<div>
+			<h1>{item.name}</h1>
+			<p>
+				Belt color is {item.belt_color} and I am {item.age} years old
+			</p>
+		</div>
+		{:else}
+			<!-- else content here -->
+			<p>
+				We have no data here ... (!-_-)
+			</p>
+	{/each}
 </main>
+
 
 <style>
 	main {
 		text-align: center;
-			padding: 1em;
+		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
