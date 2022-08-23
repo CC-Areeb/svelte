1cd172fc1e4db87fef6d3076b402e8325658f2a3
areeb <areeb.malik@cooperativecomputing.com>
2022-08-23 17:18:38 +0500

    Dispatching Custom Events

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -1,44 +1,60 @@
 <script>
+    import { createEventDispatcher } from "svelte";
+    
+    let dispatch = createEventDispatcher();
+
     let name;
     let belt_color;
     let age;
+    let weapon;
     let skills = [];
 
     const submitForm = () => {
-        console.log(name, belt_color, age, skills);
-    }
+        const person = {
+            name,
+            belt_color,
+            age,
+            skills,
+            weapon,
+            id: Math.random()
+        };
+
+        dispatch('addPerson', person);
+
+    };
 </script>
 
-<form class="modal-form" on:submit|preventDefault={submitForm}>
+<form class="modal-form" on:submit|preventDefault={submitForm} autocomplete="off">
     <input type="text" name="name" id="name" placeholder="Name" bind:value={name}>
     <input type="text" name="belt_color" id="belt_color" placeholder="Belt color" bind:value={belt_color}>
     <input type="number" name="age" id="age" placeholder="Age" bind:value={age}>
 
     <label for="skills">Skills:</label>
     <div id="skill_set">
-        <label for="">Skill 1
-            <input type="checkbox" bind:group={skills} value="Walking">
+        <label for="">Stealth
+            <input type="checkbox" bind:group={skills} value="Stealth">
         </label>
         
-        <label for="">Skill 2
+        <label for="">Talking
             <input type="checkbox" bind:group={skills} value="Talking">
         </label>
 
-        <label for="">Skill 3
+        <label for="">Sprinting
             <input type="checkbox" bind:group={skills} value="Sprinting">
         </label>
 
-        <label for="">Skill 4
+        <label for="">Jumping
             <input type="checkbox" bind:group={skills} value="Jumping">
         </label>
     </div>  
 
-    <div id="belts">
-        <select name="belt_color" id="belt_color" bind:value={belt_color}>
-            <option value="Black">Black</option>
-            <option value="Orange">Orange</option>
-            <option value="Blue">Blue</option>
-            <option value="Red">Red</option>
+    <div id="weapons">
+        <select name="weapon_class" id="weapon_class" bind:value={weapon}>
+            <option selected disabled>Choose your weapon</option>
+            <option value="Claymore">Claymore</option>
+            <option value="Sword">Sword</option>
+            <option value="Spear">Spear</option>
+            <option value="Gauntlets">Gauntlets</option>
         </select>
     </div>
 
@@ -54,8 +70,14 @@
 
     #skill_set
     {
+        margin: 20px 0px 20px 0px;
         display: flex;
         flex-direction: row;
         justify-content: space-evenly;
+    }
+
+    #weapons
+    {
+        margin: 20px 0px 20px 0px;
     }
 </style>
